0045b5d58b7d9b43bdc82992ed4cf6c116245ecf
LMO GerritBot <gerrit@libremobileos.com>
2024-09-13 04:09:22 +0000

    Adapt to LMODroid by device-add script

--- device.mk
+++ device.mk
@@ -35,7 +35,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lmodroid
 
 PRODUCT_PACKAGES += \
     xiaomi_cmi_hwlevel_default_overlay \
